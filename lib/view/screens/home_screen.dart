1300dd940a7229f696167780e5cfe519c1aa7a71
nishanpradhan <nishanpradhan500@gmail.com>
2024-10-17 08:01:09 +0545

    compeleted-home-screen

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -14,126 +14,123 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Additional header for online status and router image
-          Positioned(
-            top: 78,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 120,
-              width: double.infinity,
-              decoration: const BoxDecoration(color: AppColor.primary),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
-                    child: Text(
-                      'Good Afternoon, sumitra96_fblbr',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ),
+    return Stack(
+      children: [
+        // Additional header for online status and router image
+        Positioned(
+          top: 78,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 120,
+            width: double.infinity,
+            decoration: const BoxDecoration(color: AppColor.primary),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
+                  child: Text(
+                    'Good Afternoon, sumitra96_fblbr',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: OnlineIcon(),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: OnlineIcon(),
+                ),
+              ],
+            ),
+          ),
+        ),
+        // DraggableScrollableSheet positioned below the CustomAppbar
+        DraggableScrollableSheet(
+          initialChildSize: 0.78,
+          minChildSize: 0.78,
+          builder: (
+            BuildContext context,
+            ScrollController scrollController,
+          ) {
+            return Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                color: Colors.white, // Background color
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: ListView(
+                controller: scrollController,
+                children: [
+                  const InternetPlanCard(),
+                  const SubscriptionText(),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Heading(title: 'Quick Menu'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        QuickMenu(
+                          icons: 'assets/icons/wifi.png', // Wi-Fi icon
+                          title: 'Wi-Fi\nPassword',
+                        ),
+                        QuickMenu(
+                          icons: 'assets/icons/chat.png', // Chat icon
+                          title: 'Chat\nWith Us',
+                        ),
+                        QuickMenu(
+                          icons:
+                              'assets/icons/report.png', // Report Problem icon
+                          title: 'Report\nProblem',
+                        ),
+                        QuickMenu(
+                          icons:
+                              'assets/icons/mobile.png', // Connected Devices icon
+                          title: 'Connected\nDevices',
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: offers.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 24.0),
+                        child: OfferCard(imagePath: offers[index]),
+                      );
+                    },
                   ),
                 ],
               ),
-            ),
+            );
+          },
+        ),
+        // Custom AppBar positioned at the top
+        const Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: CustomAppbar(
+            height: 65,
           ),
-          // DraggableScrollableSheet positioned below the CustomAppbar
-          DraggableScrollableSheet(
-            initialChildSize: 0.78,
-            minChildSize: 0.78,
-            builder: (
-              BuildContext context,
-              ScrollController scrollController,
-            ) {
-              return Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height,
-                decoration: const BoxDecoration(
-                  color: Colors.white, // Background color
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                ),
-                child: ListView(
-                  controller: scrollController,
-                  children: [
-                    const InternetPlanCard(),
-                    const SubscriptionText(),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Heading(title: 'Quick Menu'),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 24.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          QuickMenu(
-                            icons: 'assets/icons/wifi.png', // Wi-Fi icon
-                            title: 'Wi-Fi\nPassword',
-                          ),
-                          QuickMenu(
-                            icons: 'assets/icons/chat.png', // Chat icon
-                            title: 'Chat\nWith Us',
-                          ),
-                          QuickMenu(
-                            icons:
-                                'assets/icons/report.png', // Report Problem icon
-                            title: 'Report\nProblem',
-                          ),
-                          QuickMenu(
-                            icons:
-                                'assets/icons/mobile.png', // Connected Devices icon
-                            title: 'Connected\nDevices',
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: offers.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(bottom: 24.0),
-                          child: OfferCard(imagePath: offers[index]),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-          // Custom AppBar positioned at the top
-          const Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: CustomAppbar(
-              height: 65,
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
