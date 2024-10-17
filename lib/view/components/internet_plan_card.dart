d0a83f62bdb07c9c85b58aff3e974099e5950cbb
nishanpradhan <nishanpradhan500@gmail.com>
2024-10-17 07:55:26 +0545

    internetplan-components

--- lib/view/components/internet_plan_card.dart
+++ lib/view/components/internet_plan_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldlink/constants/theme.dart';
 
 class InternetPlanCard extends StatelessWidget {
   const InternetPlanCard({super.key});
@@ -20,51 +21,67 @@ class InternetPlanCard extends StatelessWidget {
           color: Colors.blue[50],
           borderRadius: BorderRadius.circular(12),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Column(
-              children: [
-                Text(
-                  'My Internet Plan',
-                  style: TextStyle(
-                    fontSize: 14,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'My Internet Plan',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '98 Days left',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black87,
-                    fontWeight: FontWeight.bold,
+                  SizedBox(height: 10),
+                  Text(
+                    '98 Days left',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(172, 0, 0, 0),
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                ],
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Special Offer 80Mbps/\n12mths(A) with Dual Band\nONU',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.clip,
+                      style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+                    ),
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      height: 10, // Adjust the height here
+                      child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.circular(5), // Adjust the radius here
+                        child: LinearProgressIndicator(
+                          value: 0.5,
+                          backgroundColor: Colors.grey[300],
+                          valueColor: const AlwaysStoppedAnimation<Color>(
+                            AppColor.primary,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            const SizedBox(width: 15),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Text(
-                    'Special Offer 80Mbps/\n12mths(A) with Dual Band ONU',
-                    style: TextStyle(fontSize: 14, color: Colors.grey[700]),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                // LinearProgressIndicator(
-                //   value: 0.5,
-                //   backgroundColor: Colors.grey[300],
-                //   valueColor: const AlwaysStoppedAnimation<Color>(
-                //     Colors.blue,
-                //   ),
-                // ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
